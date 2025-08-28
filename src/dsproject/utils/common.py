57352c899290a9329cf1aeac413e23ef86f7dbfc
Abhiyendru <abhiyendru26cs003@satiengg.in>
2025-08-28 14:19:39 +0530

    File updated

--- src/dsproject/utils/common.py
+++ src/dsproject/utils/common.py
@@ -7,6 +7,7 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+from box.exceptions import BoxValueError
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
@@ -23,4 +24,12 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         Configbox: Configbox type
 
     """
-    
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {yaml_file} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("Yaml file is empty")
+    except Exception as e:
+        raise e
