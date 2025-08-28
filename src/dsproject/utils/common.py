aa4cfdf0ef17c00989857938b252d41de766b004
Abhiyendru <abhiyendru26cs003@satiengg.in>
2025-08-28 14:51:58 +0530

    File updated

--- src/dsproject/utils/common.py
+++ src/dsproject/utils/common.py
@@ -33,3 +33,35 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise ValueError("Yaml file is empty")
     except Exception as e:
         raise e
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose:True):
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+    if verbose:
+        logger.info(f"Created Directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path,"w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"Json file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded sucessfully from {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"Binary file loaded from {path}")
+    return data
