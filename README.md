0f1723408ee79a141c6cbd858dcd94daeeb8a84e
Abhiyendru <abhiyendru26cs003@satiengg.in>
2025-09-09 22:29:41 +0530

    file modified

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # End-to-end-DS-project
+
+### Workflows--ML Pipeline
+
+1. Data Ingestion
+2. Data Validation
+3. Data Transformation
+4. Model Trainer
+5. Model Evaluation
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.dsproject import logger
+from src.dsproject.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+
 
 logger.info("Welcome to the end-to-end ds project")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
