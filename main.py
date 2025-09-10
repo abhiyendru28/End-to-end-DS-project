de2aab36c42c9240ba01dbf8c9bd8d2be8994171
Abhiyendru <abhiyendru26cs003@satiengg.in>
2025-09-10 12:30:02 +0530

    Data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.dsproject import logger
 from src.dsproject.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.dsproject.pipeline.data_validation_pipeline import DataValidationPipeline
 
 
 logger.info("Welcome to the end-to-end ds project")
@@ -9,6 +10,16 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionPipeline()
     obj.initiate_data_ingestion()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationPipeline()
+    obj.initiate_data_validation()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
 except Exception as e:
     logger.exception(e)
--- src/dsproject/config/configuration.py
+++ src/dsproject/config/configuration.py
@@ -1,6 +1,6 @@
 from src.dsproject.constants import *
 from src.dsproject.utils.common import read_yaml,create_directories
-from src.dsproject.entity.config_entity import (DataIngestionConfig)
+from src.dsproject.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -25,3 +25,17 @@ class ConfigurationManager:
             unzip_dir = config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            unzip_data_dir = config.unzip_data_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            all_schema=schema,
+        )
+        return data_validation_config
