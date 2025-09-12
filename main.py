95426f933371a10bc29039e2c9e28356d6ee2d19
Abhiyendru <abhiyendru26cs003@satiengg.in>
2025-09-12 15:27:53 +0530

    Model training added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from src.dsproject import logger
 from src.dsproject.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from src.dsproject.pipeline.data_validation_pipeline import DataValidationPipeline
-
+from src.dsproject.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.dsproject.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 logger.info("Welcome to the end-to-end ds project")
 
+
 STAGE_NAME = "Data Ingestion Stage"
+
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionPipeline()
@@ -15,11 +18,36 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
 STAGE_NAME = "Data Validation Stage"
+
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationPipeline()
     obj.initiate_data_validation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerPipeline()
+    obj.initiate_model_training()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
 except Exception as e:
     logger.exception(e)
--- src/dsproject/config/configuration.py
+++ src/dsproject/config/configuration.py
@@ -1,6 +1,6 @@
 from src.dsproject.constants import *
 from src.dsproject.utils.common import read_yaml,create_directories
-from src.dsproject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
+from src.dsproject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -14,6 +14,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
         create_directories([config.root_dir])
@@ -26,6 +27,7 @@ class ConfigurationManager:
         )
         return data_ingestion_config
     
+
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
@@ -40,6 +42,7 @@ class ConfigurationManager:
         )
         return data_validation_config
     
+
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
         create_directories([config.root_dir])
@@ -49,3 +52,23 @@ class ConfigurationManager:
             data_path=config.data_path
         )
         return data_transformation_config
+    
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+        return model_trainer_config
